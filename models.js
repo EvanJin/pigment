ae5a3655a419ce35c5773f8d6a99ee2cb5452c83
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-04-12 20:41:58 +0530

    Add model to convert from and to named colors

--- models.js
+++ models.js
@@ -1,5 +1,6 @@
 var models = {};
 
+models.name = require("./models/name.js");
 models.rgb = require("./models/rgb.js");
 models.hex = require("./models/hex.js");
 models.hsl = require("./models/hsl.js");
