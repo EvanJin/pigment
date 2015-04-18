ea1c3200a0a6f05e24e26a201931302db789b12f
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-04-19 04:20:20 +0530

    Remove filter method from rgb model

--- models/rgb.js
+++ models/rgb.js
@@ -41,32 +41,6 @@ module.exports = {
         return "rgba(" + this.rgb[0] + ", " + this.rgb[1] + ", " + this.rgb[2] + ", " + this.alpha + ")";
     },
 
-    filter: function(matrix) {
-        var c = {
-                r: this.rgb[0],
-                g: this.rgb[1],
-                b: this.rgb[2],
-                a: this.alpha
-            },
-            m = matrix,
-            r, g, b, a,
-            fu = function(n) {
-                return (n < 0 ? 0 : (n < 255 ? n : 255 ));
-            };
-
-        r = ((c.r * m[0]) + (c.g * m[1]) + (c.b * m[2]) + (c.a * m[3]) + m[4]);
-        g = ((c.r * m[5]) + (c.g * m[6]) + (c.b * m[7]) + (c.a * m[8]) + m[9]);
-        b = ((c.r * m[10]) + (c.g * m[11]) + (c.b * m[12]) + (c.a * m[13]) + m[14]);
-        a = ((c.r * m[15]) + (c.g * m[16]) + (c.b * m[17]) + (c.a * m[18]) + m[19]);
-
-        return new Color({
-            red: fu(r),
-            green: fu(g),
-            blue: fu(b),
-            alpha: fu(a)
-        });
-    },
-
     luminance: function() {
         var lum = [],
             chan;
