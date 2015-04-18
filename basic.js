6cab5ea92002c532ce1f34fb131b657feb306f35
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-04-19 04:24:44 +0530

    Add a basic package

--- basic.js
+++ basic.js
@@ -5,12 +5,6 @@ models.name = require("./models/name.js");
 models.rgb = require("./models/rgb.js");
 models.hex = require("./models/hex.js");
 models.hsl = require("./models/hsl.js");
-models.hsv = require("./models/hsv.js");
-models.xyz = require("./models/xyz.js");
-models.lab = require("./models/lab.js");
-models.cmyk = require("./models/cmyk.js");
-models.schemes = require("./models/schemes.js");
-models.colorblind = require("./models/colorblind.js");
 
 for (var model in models) {
     Color.addModel(model, models[model]);
