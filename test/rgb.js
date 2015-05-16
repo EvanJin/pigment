2e4b1e1bb31620af082676ae0f314f570b9a669c
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-05-17 03:44:37 +0530

    Added tests for color manipulation in the rgb model

--- test/rgb.js
+++ test/rgb.js
@@ -18,4 +18,37 @@ describe("models:rgb", function() {
         assert.equal(c.torgb(), "rgb(240, 104, 96)");
         assert.equal(c.torgba(), "rgba(240, 104, 96, 0.3)");
     });
+
+    it("should return luminance of color", function() {
+        var c = new Color({ red: 240, green: 104, blue: 96 });
+
+        assert.equal(c.luminance(), "0.29270422282503833");
+    });
+
+    it("should return darkness of color", function() {
+        var c = new Color({ red: 240, green: 104, blue: 96 });
+
+        assert.equal(c.darkness(), "0.4362666666666666");
+    });
+
+    it("should return negative of color", function() {
+        var c = new Color({ red: 240, green: 104, blue: 96 });
+
+        assert.equal(c.negate().torgb(), "rgb(15, 151, 159)");
+    });
+
+    it("should return greyscale color", function() {
+        var c = new Color({ red: 240, green: 104, blue: 96, alpha: 0.3 });
+
+        assert.equal(c.greyscale().torgb(), "rgb(143.75199999999998, 143.75199999999998, 143.75199999999998)");
+        assert.equal(c.grayscale().torgb(), "rgb(143.75199999999998, 143.75199999999998, 143.75199999999998)");
+    });
+
+    it("should mix color", function() {
+        var c = new Color("rgb(240, 104, 96)"),
+            d = new Color("rgb(233, 240, 96)");
+
+        assert.equal(c.mix(d, 0.7).torgb(), "rgb(235, 199, 96)");
+        assert.equal(c.mix(d, 0.4).torgb(), "rgb(237, 158, 96)");
+    });
 });
